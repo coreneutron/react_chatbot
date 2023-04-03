4cff55fd2e24ecf35544513dfefd61e010557c3a
blue shark <80670036+dolphinu@users.noreply.github.com>
2023-04-03 19:50:57 -0400

    add new url

--- resources/js/constants/index.js
+++ resources/js/constants/index.js
@@ -1,3 +1,4 @@
-export const API_URL = 'https://acech.cocorofuku3.com/api'
+// export const API_URL = 'https://acech.cocorofuku3.com/api'
+export const API_URL = 'https://hikari.cocorofuku3.com/api'
 // export const API_URL = 'http://cocorofuku3.xsrv.jp/api'
 // export const API_URL = 'http://localhost:8000/api'
